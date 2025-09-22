57e1e63dc8b8831ff4c3c4794021642b5cdcf3bf
Muhammad Usman <zahoor.usman.m@gmail.com>
2025-09-22 12:18:41 +0500

    feat: Implement custom error handler

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
 import fastify from "fastify";
 import { TodoRoutes, todoRoute } from "./routes";
 import type { GeneralReply } from "./types";
-import { createApiResourcePath } from "./utils";
+import { createApiResourcePath, createFailureResponse } from "./utils";
 
 const app = fastify({
     logger: true,
+});
+
+app.setErrorHandler((error, _, reply) => {
+    app.log.error(error);
+
+    const statusCode = error.statusCode ?? 500;
+
+    reply.status(statusCode).send(createFailureResponse(error.message));
 });
 
 app.register(todoRoute, { prefix: createApiResourcePath()(TodoRoutes.base) });
